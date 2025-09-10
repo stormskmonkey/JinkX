d43108cb0af4dcc92e87ebb5f0ec1010419e7d19
stormskmonkey <113997173+stormskmonkey@users.noreply.github.com>
2025-09-11 00:27:04 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,21 +10,28 @@ local games = {
 local currentID = game.gameId
 local scriptURL = games[currentID]
 
-if getexecutorname and string.find(string.lower(tostring(getexecutorname())), "delta") then
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Executor Not Supported",
-        Text = "Delta is not supported. Please try Ronix. Join our Discord to get the Ronix executor.",
-        Duration = 30
-    })
+-- if getexecutorname and string.find(string.lower(tostring(getexecutorname())), "delta") then
+--     game:GetService("StarterGui"):SetCore("SendNotification", {
+--         Title = "Executor Not Supported",
+--         Text = "Delta is not supported. Please try Ronix. Join our Discord to get the Ronix executor.",
+--         Duration = 30
+--     })
 
-    setclipboard("https://discord.gg/XAfp5RsQ4M")
+--     setclipboard("https://discord.gg/XAfp5RsQ4M")
+--     game:GetService("StarterGui"):SetCore("SendNotification", {
+--         Title = "jinkX Notification",
+--         Text = "Discord invite link already set to your clipboard!",
+--         Duration = 30
+--     })
+--     return
+-- end
+
+ setclipboard("https://discord.gg/XAfp5RsQ4M")
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "jinkX Notification",
         Text = "Discord invite link already set to your clipboard!",
         Duration = 30
     })
-    return
-end
 
 if scriptURL then
     loadstring(game:HttpGet(scriptURL))()
