b47dc907b1bee69b2f681a2429e6f666bed71118
stormskmonkey <113997173+stormskmonkey@users.noreply.github.com>
2025-10-12 08:46:02 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,21 +1,31 @@
 repeat wait() until game:IsLoaded() and game:FindFirstChild("CoreGui") and pcall(function() return game.CoreGui end)
 
 local _function = {
+    ["block_executor"] = function()
+        if identifyexecutor and (identifyexecutor():lower():find("seliware")) then
+            return true
+        end
+        return false
+    end,
     ["getid"] = function()
         local g = game.GameId
-        if g == 6701277882 then return "e0f5b1e2af8eb6900e1324bef4e472b7"
-        elseif g == 7326934954 then return "32a295ca0700e8b3f232f8776c5e9c3e" end
+        if g == 6701277882 then return "e0f5b1e2af8eb6900e1324bef4e472b7" -- Fish It
+        elseif g == 7326934954 then return "32a295ca0700e8b3f232f8776c5e9c3e" -- 99 Night in The Forest
+        elseif g == 8009328211 then return "bf9e3c7d3db39fba6940d81c8eddedf8" -- Raise Animal
+        end
     end,
     ["gamename"] = function()
         local g = game.GameId
         if g == 6701277882 then return "Fish It"
         elseif g == 7326934954 then return "99 Night in The Forest"
+        elseif g == 8009328211 then return "Raise Animal"
         end
     end,
 }
 local keyless_script = {}
-local script_id, game_name = _function.getid(), _function.gamename()
+local script_id, game_name, block_executor = _function.getid(), _function.gamename(), _function.block_executor()
 
+if block_executor then game.Players.LocalPlayer:Kick("JinkX\nUnsupported executor detected. not supported."); return end
 if script_id then
     game.StarterGui:SetCore(
         "SendNotification",
