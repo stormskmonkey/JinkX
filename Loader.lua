265722fd144c5006066e4a624226043d216cc587
stormskmonkey <113997173+stormskmonkey@users.noreply.github.com>
2025-11-04 00:32:00 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -24,6 +24,7 @@ local _function = {
         elseif g == 7882829745 then return "b32987d20154b4aa78ef717f991146af" -- Anime Eternal
         elseif g == 8321616508 then return "a48f7af4536d0cb058f7aecb5e18007d" -- Rouge Piece
         elseif g == 7094518649 then return "9f56fe8068b5506746180cead750be8d" -- Restaurant Tycoon 3
+        elseif g == 8469926548 then return "02ffb009555e8fab6f1c5c0ac3962e46" -- Anime Fight
         end
     end,
     ["gamename"] = function()
@@ -34,6 +35,7 @@ local _function = {
         elseif g == 7882829745 then return "Anime Eternal"
         elseif g == 8321616508 then return "Rouge Piece"
         elseif g == 7094518649 then return "Restaurant Tycoon 3"
+        elseif g == 8469926548 then return "Anime Fight"
         end
     end,
 }
